7b69336678d6292655e44c9b0d63a29fed843483
Fenobi <13667739122@qq.com>
2022-11-18 18:34:14 +0800

    修改

---
+++ fu.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+
+#include "string.h"
+
+
+int main()
+{
+	//fu::test1();
+	//fu::test2();
+	//fu::test3();
+	//fu::test4();
+	//fu::test5();
+	fu::test6();
+	return 0;
+}
